95661a255ec9ed9844256e3668b2b1648a1d8f77
Daryl Guieb <dgu@hq.ivalua.com>
2018-03-15 10:54:11 -0700

    resizable file

--- js/main.js
+++ js/main.js
@@ -10,4 +10,63 @@ $(function() {
         $('.right-panel').toggleClass('collapse');
         $('.right-panel h2').fadeToggle();
     });
+  
+  $('.right-panel').resizable({
+      handles: 'n,w,s,e',
+      minWidth: 400,
+      maxWidth: 1200
+  });
+  
+  $('.close-panel').on('click', function() {
+      var panelWidth = $('.right-panel').width();
+      $('.right-panel').addClass('closed');
+      
+      $('.close-panel').hide();
+      $('.open-panel').show();
+    
+      $('.open-panel').on('click', function() {
+          $('.close-panel').show();
+          $('.open-panel').hide();
+          $('.right-panel').removeClass('closed').css('width', panelWidth);     
+      });
+  });
+  
+  $('.right-panel .menu .item').tab();
+
+  replyFunction();
+  
+  function replyFunction() {
+    $('.reply').on('click', function() {
+      $(this).parent().siblings('.feed-actions').toggle();
+    });  
+  }
+  
+  
+$('.send-button').on('click', function() {
+    var message = $('.prompt').val();
+  
+    var feed = '<div class="event"><div class="label"><img src="https://semantic-ui.com/images/avatar/small/justen.jpg"></div><div class="content"><div class="summary"><a class="user">Daryl Guieb</a><div class="date">Now</div></div><div class="extra text">' + message + '</div><div class="meta"><a class="reply">Reply</a><a class="share">Share</a></div><div class="feed-actions" style="display: none;"><div class="ui form"><div class="ui icon input"><input type="text" name="first-name" placeholder="reply"><i class="paper plane icon"></i></div></div></div></div></div>';
+  
+  if (message === '') {
+    console.log('empty comment');
+  } else {
+    $('.alert-tab .ui.feed').prepend(feed);  
+  }
+  
+  replyFunction();
+  
+  function replyFunction() {
+    $('.reply').on('click', function() {
+      $(this).parent().siblings('.feed-actions').toggle();
+    });  
+  }
+});
+  
+  $('.search-button').on('click', function() {
+    // $('.input .prompt').toggle();
+    $('.input .icon.paperclip').toggle();
+    $('.actions .send-button').toggle();
+    $('.input .search').toggleClass('active').focus();
+  });
+  
 }); 
